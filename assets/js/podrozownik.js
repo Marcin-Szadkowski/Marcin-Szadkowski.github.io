63937ed8541627c51271ae632300614a8772f41e
Marcin-Szadkowski <marcinsz9045@gmail.com>
2021-04-07 23:18:35 +0200

    Version 1.0

---
+++ assets/js/podrozownik.js
@@ -0,0 +1,20 @@
+/*===== SWIPER CAROUSEL =====*/ 
+const mySwiper = new Swiper('.hobby__container', {
+    spaceBetween: 16,
+    loop: true,
+    grabCursor: true,
+
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints:{
+        640:{
+            slidesPerView: 2,
+        },
+        1024:{
+            slidesPerView: 3,
+        }
+    }
+});
